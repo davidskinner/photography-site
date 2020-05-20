709e68b572b46b8d9484ba0d6b493e77cc8064ed
David Skinner <davidmsphoto@gmail.com>
2020-05-19 21:33:40 -0500

    home page coming along

--- photographysite/src/app/app.module.ts
+++ photographysite/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainNavigationComponent } from './main-navigation/main-navigation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainNavigationComponent
   ],
   imports: [
     BrowserModule,
