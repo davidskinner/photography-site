02f918089921bf839bcab47a1e386d4dd0abc090
David Skinner <davidmsphoto@gmail.com>
2020-06-15 21:28:52 -0500

    it helps to name things correctly

--- photographysite/src/Portfolio.ts
+++ photographysite/src/Portfolio.ts
@@ -3,8 +3,8 @@ export const portfolio =  [
             portrait: [
                 'assets/_P7C2017thicc.jpg',
                 'assets/_P7C2059-Edit-2thicc.jpg',
-                'assets/20141101-IMG_2584thicc.JPG',
-                'assets/Gilbert and Karen Gonzalez-47thicc.JPG'
+                'assets/_20141101-IMG_2584thicc.jpg',
+                'assets/Gilbert and Karen Gonzalez-47thicc.jpg'
             ],
             landscape: [
                 'limg'
