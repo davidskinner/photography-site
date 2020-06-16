d39cc2e5dc937441ece2f92b0666eee584c1fdaf
David Skinner <davidmsphoto@gmail.com>
2020-06-16 06:32:48 -0500

    first templated section put together

--- photographysite/src/Portfolio.ts
+++ photographysite/src/Portfolio.ts
@@ -1,13 +1,18 @@
 export const portfolio =  [
         {
             portrait: [
-                'assets/_P7C2017thicc.jpg',
+                'assets/IMG_0708-Editthicc.jpg',
                 'assets/_P7C2059-Edit-2thicc.jpg',
                 'assets/_20141101-IMG_2584thicc.jpg',
-                'assets/Gilbert and Karen Gonzalez-47thicc.jpg'
+                'assets/_P7C2017thicc.jpg'
             ],
-            landscape: [
-                'limg'
+            landscapeFirst: [
+                'assets/20150620-IMG_7221-Edit-2thicc.jpg',
+                'assets/IMG_2408thicc.jpg'
+            ],
+            landscapeSecond:[
+                'assets/_P7C3402thicc.jpg',
+                'assets/20140719-IMG_4263thicc.jpg'
             ]
         }
     ];
