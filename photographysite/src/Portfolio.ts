f84c005e2bcc6b3d287da07872a420088b0335db
David <dskinner@uark.edu>
2020-07-08 20:03:36 -0600

    inserted front page images

--- photographysite/src/Portfolio.ts
+++ photographysite/src/Portfolio.ts
@@ -3,8 +3,8 @@ export const portfolio =  [
             portrait: [
                 'assets/IMG_0708-Editthicc.jpg',
                 'assets/_P7C2059-Edit-2thicc.jpg',
-                'assets/_20141101-IMG_2584thicc.jpg',
-                'assets/_P7C2017thicc.jpg'
+                'assets/_P7C2017thicc.jpg',
+                'assets/_20141101-IMG_2584thicc.jpg'                
             ],
             landscapeFirst: [
                 'assets/20150620-IMG_7221-Edit-2thicc.jpg',
